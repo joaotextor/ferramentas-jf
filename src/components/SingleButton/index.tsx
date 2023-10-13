14c1382f3b77fede593faee0df828dde7346a0ea
João Textor <joaotextor@gmail.com>
2023-10-13 17:47:34 -0300

    feat: SingleButton component

---
+++ src/components/SingleButton/index.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import styles from "./SingleButton.module.css";
+import Link from "next/link";
+
+type Props = {
+  href: string;
+  imgSrc: string;
+  caption: string;
+  svgFilterCode?: string;
+};
+
+const SingleButton: React.FC<Props> = ({
+  href,
+  imgSrc,
+  caption,
+  svgFilterCode,
+  ...props
+}): JSX.Element => {
+  return (
+    <Link href={href} target="_blank">
+      <div
+        className={`flex items-center gap-1 w-fit rounded-md px-2 py-1 ${styles.buttonCss}`}
+      >
+        <div>
+          <Image
+            height={25}
+            alt="download icon"
+            src={imgSrc}
+            style={{ filter: svgFilterCode }}
+          />
+        </div>
+        <span className="text-primary-dark font-medium">{caption}</span>
+      </div>
+    </Link>
+  );
+};
+
+export default SingleButton;
