efce5b1335e9eb069fcca227ab4ce79d88a41500
João Textor <joaotextor@gmail.com>
2023-10-13 17:48:15 -0300

    configure index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,69 @@
-import Image from 'next/image'
+"use client";
+import BGParticles from "../components/BGParticles";
+import SingleButton from "@/components/SingleButton";
+import ToolCard from "@/components/ToolCard";
+
+const downloadIcon = require("../../src/images/download_icon.svg");
+const chromeIcon = require("../../src/images/chrome_icon.svg");
+const firefoxIcon = require("../../src/images/firefox_icon.svg");
 
 export default function Home() {
+  const svgFilter =
+    "invert(6%) sepia(17%) saturate(6063%) hue-rotate(198deg) brightness(90%) contrast(96%)";
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <BGParticles />
+      <main className="flex min-h-screen flex-col items-center justify-between p-12 text-justify">
+        <div>
+          <h1 className="text-center pb-8">Bem vindo, colegas!</h1>
+          <h4 className="">
+            Aqui disponibilizo algumas ferramentas para agilizar algumas tarefas
+            no dia-a-dia do nosso trabalho!
+            <div className="flex gap-2 justify-between">
+              <ToolCard
+                title="Projeto 1"
+                description="Est labore proident dolore fugiat occaecat commodo consectetur ea duis Lorem consequat fugiat ex. Excepteur magna cillum tempor magna nisi Lorem consequat anim culpa exercitation veniam. Ex ad adipisicing quis eu labore duis ea sit fugiat. Consectetur qui dolor do veniam incididunt eiusmod aliqua occaecat magna. Amet aliquip fugiat cupidatat voluptate tempor magna esse esse cillum quis. Reprehenderit sunt incididunt ut proident cillum non labore. Dolore irure nostrud adipisicing eu quis adipisicing."
+                buttons={[
+                  <SingleButton
+                    key="1"
+                    imgSrc={downloadIcon}
+                    svgFilterCode={svgFilter}
+                    href="https://www.joaotextor.com"
+                    caption="Baixar"
+                  />,
+                  <SingleButton
+                    key="2"
+                    imgSrc={chromeIcon}
+                    svgFilterCode={svgFilter}
+                    href="https://www.joaotextor.com"
+                    caption="Baixar alternativo"
+                  />,
+                ]}
+              />
+              <ToolCard
+                title="Projeto 1"
+                description="Est labore proident dolore fugiat occaecat commodo consectetur ea duis Lorem consequat fugiat ex. Excepteur magna cillum tempor magna nisi Lorem consequat anim culpa exercitation veniam. Ex ad adipisicing quis eu labore duis ea sit fugiat. Consectetur qui dolor do veniam incididunt eiusmod aliqua occaecat magna. Amet aliquip fugiat cupidatat voluptate tempor magna esse esse cillum quis. Reprehenderit sunt incididunt ut proident cillum non labore. Dolore irure nostrud adipisicing eu quis adipisicing."
+                buttons={[
+                  <SingleButton
+                    key="1"
+                    imgSrc={downloadIcon}
+                    svgFilterCode={svgFilter}
+                    href="https://www.joaotextor.com"
+                    caption="Baixar"
+                  />,
+                  <SingleButton
+                    key="2"
+                    imgSrc={chromeIcon}
+                    svgFilterCode={svgFilter}
+                    href="https://www.joaotextor.com"
+                    caption="Baixar alternativo"
+                  />,
+                ]}
+              />
+            </div>
+          </h4>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </>
+  );
 }
