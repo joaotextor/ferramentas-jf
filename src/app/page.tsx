05fa4b8a4dc46806d0f57c7d56b6a5f5c9b8aa0e
João Textor <joaotextor@gmail.com>
2023-10-14 17:40:18 -0300

    add 2 projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,11 @@ import ToolCard from "@/components/ToolCard";
 
 const downloadIcon = require("../../src/images/download_icon.svg");
 const chromeIcon = require("../../src/images/chrome_icon.svg");
+const webIcon = require("../../src/images/web_icon.svg");
 const firefoxIcon = require("../../src/images/firefox_icon.svg");
+
+const ssDiasUteis = require("../images/screenshots/diasuteis.png");
+const ssMandados = require("../images/screenshots/cumprimento_mandados.png");
 
 export default function Home() {
   const svgFilter =
@@ -13,50 +17,44 @@ export default function Home() {
   return (
     <>
       <BGParticles />
-      <main className="flex min-h-screen flex-col items-center justify-between p-12 text-justify">
+      <main className="flex min-h-screen flex-col items-center p-12 text-justify">
         <div>
-          <h1 className="text-center pb-8">Bem vindo, colegas!</h1>
+          <h1
+            className="text-center pb-8"
+            aria-label="título da página"
+            tabIndex={2}
+          >
+            Bem vindo, colegas da Justiça Federal!
+          </h1>
           <h4 className="">
             Aqui disponibilizo algumas ferramentas para agilizar algumas tarefas
             no dia-a-dia do nosso trabalho!
-            <div className="flex gap-2 justify-between">
+            <div className="flex gap-4 justify-between">
               <ToolCard
-                title="Projeto 1"
-                description="Est labore proident dolore fugiat occaecat commodo consectetur ea duis Lorem consequat fugiat ex. Excepteur magna cillum tempor magna nisi Lorem consequat anim culpa exercitation veniam. Ex ad adipisicing quis eu labore duis ea sit fugiat. Consectetur qui dolor do veniam incididunt eiusmod aliqua occaecat magna. Amet aliquip fugiat cupidatat voluptate tempor magna esse esse cillum quis. Reprehenderit sunt incididunt ut proident cillum non labore. Dolore irure nostrud adipisicing eu quis adipisicing."
+                title="Calculadora de dias úteis"
+                description="Apesar de o sistema e-Proc calcular automaticamente os dias úteis quando definimos um prazo de intimação, existem hipóteses em que necessitamos que o prazo se inicie somente a partir de determinada data (ex.: o prazo para entrega do laudo a partir da data da perícia). Esta ferramenta permite obter a data final por meio da seleção de uma data futura e a indicação do número de dias úteis a serem contados a partir dessa data. A ferramenta utiliza todos os feriados de 2023 até o final das férias advocatícias (20/01) de 2024, e considera individualmente os feriados municipais no Rio Grande do Sul (em breve também Santa Catarina e Paraná)."
+                imageUrl={ssDiasUteis}
                 buttons={[
                   <SingleButton
                     key="1"
-                    imgSrc={downloadIcon}
+                    imgSrc={webIcon}
                     svgFilterCode={svgFilter}
-                    href="https://www.joaotextor.com"
-                    caption="Baixar"
-                  />,
-                  <SingleButton
-                    key="2"
-                    imgSrc={chromeIcon}
-                    svgFilterCode={svgFilter}
-                    href="https://www.joaotextor.com"
-                    caption="Baixar alternativo"
+                    href="https://joaotextor.com/diasuteis/"
+                    caption="Acessar"
                   />,
                 ]}
               />
               <ToolCard
-                title="Projeto 1"
-                description="Est labore proident dolore fugiat occaecat commodo consectetur ea duis Lorem consequat fugiat ex. Excepteur magna cillum tempor magna nisi Lorem consequat anim culpa exercitation veniam. Ex ad adipisicing quis eu labore duis ea sit fugiat. Consectetur qui dolor do veniam incididunt eiusmod aliqua occaecat magna. Amet aliquip fugiat cupidatat voluptate tempor magna esse esse cillum quis. Reprehenderit sunt incididunt ut proident cillum non labore. Dolore irure nostrud adipisicing eu quis adipisicing."
+                title="Cumprimento de Mandados"
+                description="O Provimento/TRF4 nº informa, em seu Anexo VI, a lista de municípios atendidos pela JF da 4º Região, se há cumprimento de mandados e qual Subseção ou UAA é responsável pelo cumprimento. Diante da imensa quantidade de dados na tabela, perde-se tempo para identificar se em determinado município há ou não cumprimento de mandados. Esta ferramenta visa entregar essa informação de forma simplicada, bastando ao usuário digitar o nome da cidade desejada (No momento apenas os municípios do Rio Grande do Sul estão disponíveis)."
+                imageUrl={ssMandados}
                 buttons={[
                   <SingleButton
                     key="1"
-                    imgSrc={downloadIcon}
+                    imgSrc={webIcon}
                     svgFilterCode={svgFilter}
-                    href="https://www.joaotextor.com"
-                    caption="Baixar"
-                  />,
-                  <SingleButton
-                    key="2"
-                    imgSrc={chromeIcon}
-                    svgFilterCode={svgFilter}
-                    href="https://www.joaotextor.com"
-                    caption="Baixar alternativo"
+                    href="https://joaotextor.com/cumprimento-mandados/"
+                    caption="Acessar"
                   />,
                 ]}
               />
--- src/components/ToolCard/index.tsx
+++ src/components/ToolCard/index.tsx
@@ -1,20 +1,27 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
+import { ImgHTMLAttributes } from "react";
 
 type Props = {
   title: string;
   description: string;
+  imageUrl?: string | StaticImport;
   buttons?: React.ReactNode;
 };
 
 const ToolCard: React.FC<Props> = ({
   title,
   description,
+  imageUrl,
   buttons,
   ...props
 }) => {
   return (
-    <div className="my-3 text-center max-w-md w-fit rounded-3xl border-primary-light border-2 p-3 leading-none">
+    <div className="my-3 text-center max-w-md w-fit h-fit rounded-3xl border-2 p-3 leading-none bg-gradient-to-b from-sky-950 to-primary-dark shadow-lg shadow-slate-500">
       <h1 className="mb-4">{title}</h1>
+      <div className="mb-3 flex justify-center">
+        <Image src={imageUrl!} alt={title} height={250} />
+      </div>
       <span className="text-base leading-normal">{description}</span>
       <div className="flex gap-2 pt-5 justify-center">{buttons}</div>
     </div>
